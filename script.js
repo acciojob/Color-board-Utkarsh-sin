a9b94c6d86f690ba46027721c42919b9077e924a
Utkarsh-sin <56604999+Utkarsh-sin@users.noreply.github.com>
2024-03-20 00:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'red';
+        setTimeout(() => {
+            square.style.backgroundColor = '';
+        }, 1000);
+    });
+});
